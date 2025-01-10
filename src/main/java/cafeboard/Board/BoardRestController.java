0011340364212ddeb6c9bc591ca019b3ff92a385
YounTaeWoo <tty7797@naver.com>
2025-01-10 15:05:07 +0900

    feat: 게시판 조회 기능 구현

--- src/main/java/cafeboard/Board/BoardRestController.java
+++ src/main/java/cafeboard/Board/BoardRestController.java
@@ -2,9 +2,13 @@ package cafeboard.Board;
 
 import cafeboard.Board.Dto.CreateBoard;
 import cafeboard.Board.Dto.CreateBoardResponse;
+import cafeboard.Board.Dto.FindAllResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BoardRestController {
@@ -19,5 +23,11 @@ public class BoardRestController {
     @PostMapping("/boards")
     public CreateBoardResponse createBoard(@RequestBody CreateBoard createBoard) {
         return boardService.createBoard(createBoard);
+    }
+
+    // 게시판 모두 조회
+    @GetMapping("/boards")
+    public List<FindAllResponse> findAll() {
+        return boardService.findAll();
     }
 }
--- src/main/java/cafeboard/Board/BoardService.java
+++ src/main/java/cafeboard/Board/BoardService.java
@@ -2,9 +2,11 @@ package cafeboard.Board;
 
 import cafeboard.Board.Dto.CreateBoard;
 import cafeboard.Board.Dto.CreateBoardResponse;
+import cafeboard.Board.Dto.FindAllResponse;
 import org.springframework.stereotype.Service;
 
 import javax.swing.plaf.PanelUI;
+import java.util.List;
 
 @Service
 public class BoardService {
@@ -19,5 +21,13 @@ public class BoardService {
         Board board = new Board(createBoard.title());
         boardRepository.save(board);
         return new CreateBoardResponse(board.getTitle(), board.getCreatedTime(), board.getId());
+    }
+
+    // 게시판 모두 조회
+    public List<FindAllResponse> findAll() {
+        List<Board> boards = boardRepository.findAll();
+        return boards.stream()
+                .map(board -> new FindAllResponse(board.getTitle(), board.getId()))
+                .toList();
     }
 }
