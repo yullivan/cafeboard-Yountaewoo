38a862dcc682752d6c6ee033f7c6f3cc52fa5364
YounTaeWoo <tty7797@naver.com>
2025-01-10 14:55:38 +0900

    feat: 게시판 생성 수정(auditing 적용 추가)

--- src/main/java/cafeboard/Board/Board.java
+++ src/main/java/cafeboard/Board/Board.java
@@ -1,15 +1,14 @@
 package cafeboard.Board;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Board {
 
     private String title;
