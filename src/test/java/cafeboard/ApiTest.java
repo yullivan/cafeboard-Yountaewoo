8613729f2b20e5fb5589e02e0e456cf239db5103
YounTaeWoo <tty7797@naver.com>
2025-01-13 12:07:30 +0900

    feat: 댓글 삭제 API Test

--- src/test/java/cafeboard/ApiTest.java
+++ src/test/java/cafeboard/ApiTest.java
@@ -199,4 +199,49 @@ public class ApiTest {
         assertThat(updateComment.content()).isEqualTo("와우!");
 
     }
+
+    @Test
+    void 댓글삭제ApiTest() {
+        //게시판 생성
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new CreateBoard("자유게시판"))
+                .when()
+                .post("/boards")
+                .then().log().all()
+                .statusCode(200); // 요청에 대한 서버 응답의 상태코드가 200인지 검증
+
+        // 게시글 생성
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new CreatePost("점메추", "점심메뉴추천", 1L))
+                .when()
+                .post("/posts")
+                .then().log().all()
+                .statusCode(200);
+
+
+        //댓글 생성
+        CreateCommentResponse createCommentResponse = RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new CreateComment("우와!", 1L))
+                .when()
+                .post("/comments")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(CreateCommentResponse.class);
+
+        //댓글 삭제
+        RestAssured
+                .given().log().all()
+                .pathParam("commentId", 1L)
+                .when()
+                .delete("/comments/{commentId}")
+                .then().log().all()
+                .statusCode(200);
+    }
 }
