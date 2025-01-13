93bee48a54d4ee10327c851aae29de4dfc86e681
YounTaeWoo <tty7797@naver.com>
2025-01-13 16:28:04 +0900

    feat: 게시글 삭제 Api Test

--- src/test/java/cafeboard/ApiTest.java
+++ src/test/java/cafeboard/ApiTest.java
@@ -370,7 +370,49 @@ public class ApiTest {
                 .extract()
                 .as(UpdatePostResponse.class);
 
-
         assertThat(updatePostResponse.content()).isEqualTo("저녁메뉴추천");
+    }
+
+    @Test
+    void 게시글삭제ApiTest() {
+        //게시판 생성
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new CreateBoard("자유게시판"))
+                .when()
+                .post("/boards")
+                .then().log().all()
+                .statusCode(200);
+
+        // 게시글 생성
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new CreatePost("점메추", "점심메뉴추천", 1L))
+                .when()
+                .post("/posts")
+                .then().log().all()
+                .statusCode(200);
+
+        //게시글 삭제
+        RestAssured
+                .given().log().all()
+                .pathParam("postId", 1L)
+                .when()
+                .delete("/posts/{postId}")
+                .then().log().all()
+                .statusCode(200);
+
+        //게시글 목록 조회
+        FindAllPostsResponse findAllPostsResponse = RestAssured
+                .given().log().all()
+                .when()
+                .get("/posts")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(FindAllPostsResponse.class);
+        assertThat(findAllPostsResponse.posts().size()).isEqualTo(0);
     }
 }
