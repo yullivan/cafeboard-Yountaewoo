f5366316409942c02b06abd53e3284dd4fcfbe31
YounTaeWoo <tty7797@naver.com>
2025-01-13 15:29:07 +0900

    feat: 게시글 목록 조회 Api Test

--- src/test/java/cafeboard/ApiTest.java
+++ src/test/java/cafeboard/ApiTest.java
@@ -8,6 +8,7 @@ import cafeboard.Comment.DTO.CreateCommentResponse;
 import cafeboard.Comment.DTO.UpdateComment;
 import cafeboard.Comment.DTO.UpdateCommentResponse;
 import cafeboard.Post.DTO.CreatePost;
+import cafeboard.Post.DTO.FindAllPostsResponse;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
@@ -243,5 +244,50 @@ public class ApiTest {
                 .delete("/comments/{commentId}")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    void 게시글목록조회ApiTest() {
+        //게시판 생성
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new CreateBoard("자유게시판"))
+                .when()
+                .post("/boards")
+                .then().log().all()
+                .statusCode(200);
+
+        // 게시글 생성
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new CreatePost("점메추", "점심메뉴추천", 1L))
+                .when()
+                .post("/posts")
+                .then().log().all()
+                .statusCode(200);
+
+        //댓글 생성
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .body(new CreateComment("우와!", 1L))
+                .when()
+                .post("/comments")
+                .then().log().all()
+                .statusCode(200);
+
+        // 댓글 조회
+        FindAllPostsResponse findAllPostsResponse = RestAssured
+                .given().log().all()
+                .when()
+                .get("/posts")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(FindAllPostsResponse.class);
+
+        assertThat(findAllPostsResponse.posts().size()).isEqualTo(1);
     }
 }
